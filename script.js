7a79fefd109860fec447350292c3650f860f2f20
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-10 15:06:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let btn = document.getElementById("enterBtn")
+let status = document.getElementById("status")
+
+btn.addEventListener("click", ()=>{
+	status.outerHTML = "<h1>Enter Metaverse</h1>"
+})
